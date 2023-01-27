7b4e2709d4d7bde64d875d6da76e12a4745a2900
Lakarteux <alescobarpinzon@gmail.com>
2023-01-26 21:27:29 -0500

    se añaden todos los archivos del proyecto

---
+++ mapa.js
@@ -0,0 +1,51 @@
+//Funciónes para cambiar el contenido del html dentro de un elemento con un ID
+// Ademas especifican que se scrollee a ese elemento al ejecutarse la función
+
+function Altus() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind13.jpg" alt="punto uno"><br><p class="text-info"><span class="text-title">Meseta Altus</span><br><br>Esta es una zona que se ubica hacia arriba del mapa, arriba y a la derecha de Liurnia de los Lagos "Donde se puede encontrar la base del Árbol Áureo y sirve como entrada a la capital real, Leyndell. Cicatrices de la fragmentación y la guerra permanecen aparentes hasta este día" </p>';
+	document.getElementById("Info").scrollIntoView();
+	
+}
+
+
+function Caelid() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind2.jpg" alt="punto dos"><br><p class="text-info"><span class="text-title">Caelid</span><br><br>Esta es una zona que se ubica hacia abajo del mapa, a la derecha de Necrolimbo "Un yermo árido que se ve afectado por la podredumbre escarlata, mutando su flora y fauna, bañando el ambiente en un tono mortal.  Muerte y decadencia son vistas muy frecuentes aquí." </p>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+
+function Consecrated() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind20.jpg" alt="punto tres"><br><p class="text-info"><span class="text-title">Campo de Nieve Consagrado</span><br><br>Esta es una zona que se ubica hacia arriba del mapa, al lado izquierdo de Pico de los Gigantes "La tierra prometida de los Albinauricos yace más allá, y aquellos que enfrentan a sus sombríos asesinos, pueden obtener acceso al Árbol Hierático de Miquella"</p>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+
+function Dragon() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind10.jpg" alt="punto cuatro"><br><p class="text-info"><span class="text-title">Tumulo de Dragones</span><br><br>Esta es una zona que se ubica hacia la mitad del mapa, arriba de Caelid "Es una gran extensión de mesetas aislada de la masa principal de Caelid. Es un santuario creado por los dragones que escaparon de la podredumbre escarlata que asola las tierras del sur"</p>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+function Limb() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/indeximg.jpg" alt="punto cuatro"><br><p class="text-info"><span class="text-title">Necrolimbo</span><br><br>Esta es una zona que se ubica hacia abajo del mapa. "Es una zona exuberante y expansiva del Tenebrae Demesne. Arboles dorados, hierba alta, y arbustos proveen suficiente sustento a la fauna local, que incluye jabalíes, ovejas, cabras y roedores además de criaturas voladoras como águilas y búhos. Fauna más siniestra y agresiva también existe, y los que se aventuren deben estar preparados para combatirlos."</p>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+function Liurnia() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind7.jpg" alt="punto cuatro"><br><p class="text-info"><span class="text-title">Liurnia de los Lagos</span><br><br>Esta es una zona que se ubica hacia la mitad del mapa, arriba de Necrolimbo "Con sus aguas poco profundas y vastos humedales, la región de Liurnia es acosada por el hundimiento gradual de la mayoría de su masa.  Con sus bosques perpetuamente cubiertos de niebla, se pueden escuchar sonidos espeluznantes de campanas en la distancia."</p>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+function Gelmir() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind17.jpg" alt="punto cuatro"><br><p class="text-info"><span class="text-title">Monte Gelmir</span><br><br>Esta es una zona que se ubica hacia arriba del mapa, arriba de Liurnia de los Lagos "Es una región volcánica al oeste de la Meseta Altus. Esta tierra ha visto muchas batallas y la lucha continua"</p>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+function Mountain() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind19.jpg" alt="punto cuatro"><br><p class="text-info"><span class="text-title">Pico de los Gigantes</span><br><br>Esta es una zona que se ubica hacia arriba del mapa, encima de Meseta Altus "Las tierras de los gigantes no son más. Devastadas por la Fragmentación, su forja yace fría y sus cuerpos congelados están esparcidos por la región desolada, ahora solo habitable por espíritus"</p>';
+	document.getElementById("Info").scrollIntoView();
+}
+
+function Weeping() {
+	document.getElementById("Info").innerHTML = '<img class="imgmapinfo" src="images/ind11.jpg" alt="punto cuatro"><br><p class="text-info"><span class="text-title">Península del Llanto</span><br><br>Esta es una zona que se ubica hacia abajo del mapa. "Es una larga península que se desprende de la masa de tierra principal de Necrolimbo en el sur, conectada solo por el Puente de Sacrificio. Es una tierra hosca, acosada por una atmosfera de culpa y perdida. El asediado Castillo Morne yace en ruinas al sur."</p>';
+	document.getElementById("Info").scrollIntoView();
+}
